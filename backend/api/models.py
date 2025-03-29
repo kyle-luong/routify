ac917bf34f3b650ca34341b0c0b69c89f8cfc02f
kyle-luong <133829822+kyle-luong@users.noreply.github.com>
2025-03-29 15:23:21 -0400

    models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Session(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Event(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='events')
+    title = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    weekday = models.CharField(max_length=10)
