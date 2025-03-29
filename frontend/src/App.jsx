410beaa9b7c2462ae877b9bf8b2c923902c148ba
Jonathan Lam <jonhaolam@gmail.com>
2025-03-29 17:06:01 -0400

    Added route to homepage and removed boilerplate code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,17 @@
-import logo from './assets/logo.svg';
 import './styles/index.css';
 import './styles/App.css';
+import HomePage from './pages/HomePage.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Routes>
+            {/* root page directs to home page */}
+            <Route path="/" element={<HomePage />} />                        
+          </Routes>
+        </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/HomePage.css';
+// import Navbar from '../components/Navbar.jsx';
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <p>this is the home page!</p>
+        </div>
+    );
+};
+
+export default HomePage;
