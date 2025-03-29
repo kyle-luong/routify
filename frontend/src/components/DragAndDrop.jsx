6a57e9fdcc7a48eff24ddbfddfa70f8b24a0bb87
Jonathan Lam <jonhaolam@gmail.com>
2025-03-29 17:46:42 -0400

    Implemented DragAndDrop functionality

--- frontend/src/components/DragAndDrop.jsx
+++ frontend/src/components/DragAndDrop.jsx
@@ -1,11 +1,68 @@
-import React from "react";
+/* Inspired from https://medium.com/@dprincecoder/creating-a-drag-and-drop-file-upload-component-in-react-a-step-by-step-guide-4d93b6cc21e0 */
 
-const DragAndDrop = () => {
-    return (
-        <div>
-            this is the drag and drop area!
+import React, { useEffect, useState } from "react";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { MdClear } from "react-icons/md";
+import "../styles/DragAndDrop.css";
+
+const DragAndDrop = ({ onFilesSelected, width = "100%", height = "200px" }) => {
+  const [files, setFiles] = useState([]);
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const droppedFiles = event.dataTransfer.files;
+    if (droppedFiles.length > 0) {
+      const newFiles = Array.from(droppedFiles);
+      setFiles((prevFiles) => [...prevFiles, ...newFiles]);
+    }
+  };
+
+  const handleRemoveFile = (index) => {
+    setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+  };
+
+  useEffect(() => {
+    if (onFilesSelected) {
+      onFilesSelected(files);
+    }
+  }, [files, onFilesSelected]);
+
+  return (
+    <section className="drag-drop" style={{ width, height }}>
+      <div
+        className={`document-uploader ${files.length > 0 ? "upload-box active" : "upload-box"}`}
+        onDrop={handleDrop}
+        onDragOver={(event) => event.preventDefault()}
+      >
+        <div className="upload-info">
+          <AiOutlineCloudUpload />
+          <div>
+            <p>Drag and drop your files here</p>
+            <p>
+              Supported files: .JPG, .PNG, .JPEG, .PDF, .DOCX, .PPTX, .TXT, .XLSX
+            </p>
+          </div>
         </div>
-    )
-}
+
+        {files.length > 0 && (
+          <div className="file-list">
+            <div className="file-list__container">
+              {files.map((file, index) => (
+                <div className="file-item" key={index}>
+                  <div className="file-info">
+                    <p>{file.name}</p>
+                  </div>
+                  <div className="file-actions">
+                    <MdClear onClick={() => handleRemoveFile(index)} />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default DragAndDrop;
