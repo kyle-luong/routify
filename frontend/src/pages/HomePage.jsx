3426d677fb763b58d45d3800e5660042672bb82e
Jonathan Lam <jonhaolam@gmail.com>
2025-03-29 17:29:37 -0400

    Created Navbar component and added basic styling

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../styles/HomePage.css';
-// import Navbar from '../components/Navbar.jsx';
+import Navbar from '../components/Navbar.jsx';
 
 const HomePage = () => {
     return (
         <div className="home-page">
-            <p>this is the home page!</p>
+            <Navbar />
         </div>
     );
 };
