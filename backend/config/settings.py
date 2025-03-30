ebdae8382449b5d974ec6528656da5156f1d75ab
kyle-luong <133829822+kyle-luong@users.noreply.github.com>
2025-03-29 21:37:34 -0400

    schedule page

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-lod&ovozm)#t3tdp8lok!1dy%s=%9oo2brizah+tja63r4*a&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    'rest_framework',
+    'corsheaders',
+
     "api",
 ]
 
@@ -49,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -123,3 +128,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
